98e27c9f20f1909e9210bd1cf9438e4f1c41449a
ohminsu99 <ohminsu99@gmail.com>
2023-06-16 09:21:01 +0900

    test ver 1.1.1 (프리즈마 json 개발 의존성 이동)

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,14 +1,28 @@
+'use client';
+
+import { useState, ChangeEvent } from 'react';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default function Login() {
+export default function NoteForm() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  };
+
   const createNote = async () => {
     try {
       const newNote = await prisma.notes.create({
         data: {
-          title: '할 일 제목',
-          content: '할 일 내용',
+          title,
+          content,
         },
       });
       console.log('새로운 할 일이 생성되었습니다:', newNote);
@@ -19,6 +33,8 @@ export default function Login() {
 
   return (
     <div>
+      <input type="text" value={title} onChange={handleTitleChange} placeholder="할 일 제목" />
+      <textarea value={content} onChange={handleContentChange} placeholder="할 일 내용" />
       <button onClick={createNote}>할 일 생성</button>
     </div>
   );
